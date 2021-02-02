c5baef1c74ea5921df14e57a8275dab46c89b4f1
Alan Secundo <alan_secundo@hotmail.com>
2021-02-02 16:05:02 -0300

    Added flip transition for a better user experience

--- app.js
+++ app.js
@@ -1,74 +1,67 @@
 let isVisible = false;
 let cards = [
   {
-    id: 'A1',
-    img: './assets/rick.png'
+    id: "A1",
+    img: "./assets/rick.png",
   },
   {
-    id: 'A2',
-    img: './assets/rick.png'
+    id: "A2",
+    img: "./assets/rick.png",
   },
   {
-    id: 'B1',
-    img: './assets/pickle-rick.png'
+    id: "B1",
+    img: "./assets/pickle-rick.png",
   },
   {
-    id: 'B2',
-    img: './assets/pickle-rick.png'
+    id: "B2",
+    img: "./assets/pickle-rick.png",
   },
   {
-    id: 'C1',
-    img: './assets/morty-fake.png'
+    id: "C1",
+    img: "./assets/morty-fake.png",
   },
   {
-    id: 'C2',
-    img: './assets/morty-fake.png'
+    id: "C2",
+    img: "./assets/morty-fake.png",
   },
   {
-    id: 'D1',
-    img: './assets/morty.png'
+    id: "D1",
+    img: "./assets/morty.png",
   },
   {
-    id: 'D2',
-    img: './assets/morty.png'
+    id: "D2",
+    img: "./assets/morty.png",
   },
   {
-    id: 'E1',
-    img: './assets/dog.png'
+    id: "E1",
+    img: "./assets/dog.png",
   },
   {
-    id: 'E2',
-    img: './assets/dog.png'
+    id: "E2",
+    img: "./assets/dog.png",
   },
   {
-    id: 'F1',
-    img: './assets/sir.png'
+    id: "F1",
+    img: "./assets/sir.png",
   },
   {
-    id: 'F2',
-    img: './assets/sir.png'
+    id: "F2",
+    img: "./assets/sir.png",
   },
-] ;
-// let cards = ["A1", "A2", "B1", "B2", "C1", "C2", "D1", "D2", "E1", "E2"];
+];
 let answer = "";
 let points = 0;
 let moves = 0;
 
-const colorMatch = '#12b0c8'
+const colorMatch = "#12b0c8";
 
-function show(id) {
-  document.getElementById(id).style.display = "flex";
-  document.getElementById(id).style.display = "flex";
-  isVisible = true;
-}
-
-function hidden(id) {
-  document.getElementById(id).style.display = "none";
-  isVisible = false;
+function flip(id) {
+  var flip = document.getElementById(id);
+  flip.classList.toggle("is-flipped");
 }
 
 function finishGame() {
-  return points === cards.length / 2 ? true : false;
+  return points === cards.length / 2;
 }
 
 function incrementMove() {
@@ -80,9 +73,9 @@ function changeVisibility(id) {
   let actualElement = document.getElementById(id);
   let lastElement = document.getElementById(answer);
 
-  if (answer === "") {
+  if (!answer) {
     answer = id;
-    show(id);
+    flip(id);
     return;
   }
 
@@ -91,28 +84,30 @@ function changeVisibility(id) {
   }
 
   if (id.substring(0, 1) === answer.substring(0, 1)) {
-    show(id);
-    lastElement.style.backgroundColor = colorMatch;
-    actualElement.style.backgroundColor = colorMatch;
+    flip(id);
     lastElement.onclick = null;
     actualElement.onclick = null;
     answer = "";
     points = points + 1;
     incrementMove();
     document.getElementById("points").innerHTML = points;
+    setTimeout(() => {
+      lastElement.style.backgroundColor = colorMatch;
+      actualElement.style.backgroundColor = colorMatch;
+    }, 800);
     if (finishGame()) {
       setTimeout(() => {
         alert("YOU WON!");
       }, 700);
     }
   } else {
-    show(id);
+    flip(id);
     setTimeout(() => {
-      hidden(id);
-      hidden(answer);
+      flip(id);
+      flip(answer);
       answer = "";
       incrementMove();
-    }, 500);
+    }, 1100);
   }
 }
 
@@ -137,7 +132,6 @@ function configureImage(element, url) {
   element.setAttribute("display", "block");
   element.setAttribute("width", "130px");
   element.setAttribute("height", "130px");
-
 }
 
 function createGame(list) {
@@ -150,18 +144,23 @@ function createGame(list) {
 
   list.forEach((element) => {
     const div = document.createElement("div");
-    const internalDiv = document.createElement("div");
     const img = document.createElement("IMG");
     configureImage(img, element.img);
 
+    const divFront = document.createElement("div");
+    const divBack = document.createElement("div");
+    divFront.setAttribute("class", "card__face");
+    divBack.setAttribute("class", "card__back card__face");
+
     div.setAttribute("class", "section__card");
-    internalDiv.setAttribute("id", element.id);
+    div.setAttribute("id", element.id);
     div.onclick = () => changeVisibility(element.id);
 
     document
       .getElementById("game-content")
       .appendChild(div)
-      .appendChild(internalDiv)
+      .appendChild(divFront)
+      .parentNode.insertBefore(divBack, divFront.nextSibling)
       .appendChild(img);
   });
 }
